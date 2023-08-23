d0f4b22ed328ce0e5298565264cd67db9bb326f7
wdd <2437767452@qq.com>
2023-08-23 16:35:02 +0800

    add hello.c

---
+++ hello.c
@@ -0,0 +1,12 @@
+/*************************************************************************
+	> File Name: hello.c
+	> Author: 
+	> Mail: 
+	> Created Time: Wed 23 Aug 2023 04:33:42 PM CST
+ ************************************************************************/
+
+#include<stdio.h>
+int main() {
+    printf("hello my github!\n");
+    return 0;
+}
