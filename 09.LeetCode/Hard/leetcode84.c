352367b918c8b5040ce0cb79893bd3894db2a9e2
wdd <2437767452@qq.com>
2023-09-04 17:08:05 +0800

    修改了leetcode84题单调栈解法

--- 09.LeetCode/Hard/leetcode84.c
+++ 09.LeetCode/Hard/leetcode84.c
@@ -31,12 +31,47 @@ int largestRectangleArea(int* heights, int heightsSize){
     return ans;
 }
 
+//单调栈
+int largestRectangleArea_1(int *heights, int heightsSize) { 
+    int ans = 0;
+    int stack[heightsSize + 5], top = -1;
+    //1、遍历数组中的元素
+    for (int i = 0; i < heightsSize; i++) {
+        //1.1、采用栈的性质，当栈不为空时比较栈顶元素与第i个元素的大小
+        while ( top != -1 && heights[stack[top]] > heights[i]) {
+            //1.1.1、代码到这说明栈结构的单调性被破坏，栈顶元素需要进行出栈并计算面积大小。
+            int length = heights[stack[top--]];
+            //1.1.2、当栈结构为空了，说明当前元素是已经遍历过元素中最小的，因此它的宽度就等于它的下标
+            int weight = i;
+            if (top != -1) {
+                weight = i - stack[top] - 1;
+            }
+            //1.1.3、计算比较当前面积与前面最大结果的大小
+            ans = fmax(ans, length * weight);
+        }
+        stack[++top] = i;
+    } 
+    //2、防止在栈结构中还有元素存在。
+    while (top != -1) {
+        int length = heights[stack[top--]];
+        //2.1、当栈为空时，说明栈顶元素的大小应该是整个数组最小的
+        int weight = heightsSize;
+        if (top != -1) {
+            weight = heightsSize - stack[top] - 1;
+        }
+        ans = fmax(ans, length * weight);
+    }
+    return ans;
+}
+
+
 int main() {
     int arr[MAX + 5] = {0}, n;
     scanf("%d", &n);
     for (int i = 0; i < n; i++) {
         scanf("%d", arr + i);
     }
-    printf("ans = %d\n", largestRectangleArea(arr, n));
+    printf("暴力解法 = %d\n", largestRectangleArea(arr, n));
+    printf("单调栈解法 = %d\n", largestRectangleArea_1(arr, n));
     return 0;
 }
