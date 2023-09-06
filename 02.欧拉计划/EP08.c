d0b46d2eb4d1ae60395db6975f4ac007c70022e9
wdd <2437767452@qq.com>
2023-09-06 21:10:27 +0800

    上传了欧拉计划第8题

--- 02.欧拉计划/EP08.c
+++ 02.欧拉计划/EP08.c
@@ -6,29 +6,45 @@
  ************************************************************************/
 
 #include<stdio.h>
-
+#include<stdlib.h>
+#include<string.h>
 //欧拉计划第八题
 //滑动窗法
 
 int main() {
     
-    int ans = 0, k = 0, ret = 0, zero = 0;
-    int arr[1005];
-    for (int i = 0; i < 1000; i++) scanf("%d", &arr[i]);
+    int arr[1005], i = 0;
+    char buff[1000] = {0}; 
+    int n = 0;
+    //读取数据
+    while (~scanf("%s", buff)) {
+        for (int i = 0; buff[i]; i++) {
+          arr[n++]  = buff[i] - '0';
+        }
+        memset(buff, 0, sizeof(buff));
+    }
     
-    for (int i = 0; i < 1000; i++) {
-        if (arr[i] == 0) {
-            zero++;
-            k++;
-            continue;
+    //处理数据
+    long long ans = 0, k = 13, tmp = 1, zero_cnt = 0;
+    //滑动窗口法
+    for (int i = 0; i < n; i++) {
+        if (arr[i] != 0) {
+            tmp *= arr[i]; 
+        } else {
+            zero_cnt++;
         }
-        ret *= arr[i];
-        if (k < 4) continue;
-        if (arr[i - k] == 0) zero--;
-        else {
-            
+        if (i < k) continue;
+
+        if (arr[i - k] == 0) {
+            zero_cnt--;
+        } else {
+            tmp /= arr[i - k];
         }
+        
+        if (!zero_cnt && ans < tmp) ans = tmp;
 
     }
+
+    printf("result = %lld\n", ans);
     return 0;
 }
