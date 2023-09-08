a33b4d4420c398984ef7d206ebd7ed19f889afd5
wdd <2437767452@qq.com>
2023-09-08 20:09:29 +0800

    add 插入排序

--- 03.基础数据结构/05.排序与查找/01.stable_sort.c
+++ 03.基础数据结构/05.排序与查找/01.stable_sort.c
@@ -30,17 +30,30 @@
 
 //稳定排序：冒泡、插入、希尔、基数
 
-void bubble(int *nums, int n) {
+void bubble_sort(int *nums, int n) {
     //冒泡排序
     int flag = 0;
     for (int i = 1; i <= n - 1; i++) {
+        flag = 0;
         for (int j = 0; j < n - i + 1; j++) {
-            if (nums[j - 1] > nums[j]) {
+           if (nums[j - 1] > nums[j]) {
                 swap(nums[j], nums[j - 1])
+                flag = 1;
             }
             
         }
-   //     if (!flag) break;
+        if (!flag) break;
+    }
+    return ;
+}
+
+//插入排序
+void insert_sort(int *nums, int n) {
+    for (int i = 0; i < n; i++) {
+        for (int j = i; j >= 1 && nums[j] < nums[j - 1]; j--) {
+            swap(nums[j], nums[j - 1]);
+        }
+
     }
     return ;
 }
@@ -53,7 +66,8 @@ int main() {
     for (int i = 0; i < max_op; i++) {
         num[i] = rand() % 100; 
     }
-    TEST(num, bubble, max_op);
+    TEST(num, bubble_sort, max_op);
+    TEST(num, insert_sort, max_op);
     #undef max_op
     return 0;
 }
