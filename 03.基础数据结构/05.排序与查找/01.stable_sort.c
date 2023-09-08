ca633b8f8cd28094b8b2c2e6848a806b7d0866f3
wdd <2437767452@qq.com>
2023-09-08 19:58:38 +0800

    增加了稳定排序-冒泡排序

---
+++ 03.基础数据结构/05.排序与查找/01.stable_sort.c
@@ -0,0 +1,59 @@
+/*************************************************************************
+	> File Name: 01.stable_sort.c
+	> Author: 
+	> Mail: 
+	> Created Time: Fri 08 Sep 2023 07:12:05 PM CST
+ ************************************************************************/
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<time.h>
+
+
+#define TEST(arr, func, n) {\
+    int *nums = (int *)malloc(sizeof(int) * n);\
+    memcpy(nums, arr, sizeof(int) * n);\
+    func(nums, n);\
+    printf("[");\
+    for (int i = 0; i < n; i++) {\
+        i && printf(" ");\
+        printf("%d", nums[i]);\
+    }\
+    printf("] <=== %s[%d]\n", #func, n);\
+    free(nums);\
+}
+
+#define swap(a, b) {\
+    __typeof(a) __tmp = a; a = b; b = __tmp;\
+}
+
+//稳定排序：冒泡、插入、希尔、基数
+
+void bubble(int *nums, int n) {
+    //冒泡排序
+    int flag = 0;
+    for (int i = 1; i <= n - 1; i++) {
+        for (int j = 0; j < n - i + 1; j++) {
+            if (nums[j - 1] > nums[j]) {
+                swap(nums[j], nums[j - 1])
+            }
+            
+        }
+   //     if (!flag) break;
+    }
+    return ;
+}
+
+
+int main() {
+    int num[1000];
+    #define max_op 20
+    srand(time(0));
+    for (int i = 0; i < max_op; i++) {
+        num[i] = rand() % 100; 
+    }
+    TEST(num, bubble, max_op);
+    #undef max_op
+    return 0;
+}
