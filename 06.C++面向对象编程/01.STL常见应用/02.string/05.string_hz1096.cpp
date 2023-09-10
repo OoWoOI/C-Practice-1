d2263beb9f18d18c5210cd4a0d2869fc2e5fdf29
wdd <2437767452@qq.com>
2023-09-10 20:47:36 +0800

    跟新了C++的STL板块目录string1096

---
+++ 06.C++面向对象编程/01.STL常见应用/02.string/05.string_hz1096.cpp
@@ -0,0 +1,19 @@
+/*************************************************************************
+	> File Name: 05.string_hz1096.cpp
+	> Author: 
+	> Mail: 
+	> Created Time: Sun 10 Sep 2023 08:41:01 PM CST
+ ************************************************************************/
+
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main() {
+    string s;
+    cin >> s;
+    cout << s.length() << " ";
+    s.clear();
+    cout << s.length() << endl;
+    return 0;
+}
