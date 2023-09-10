a021bfd68bbcffbd55dba0c4273bf9a4c64f3155
wdd <2437767452@qq.com>
2023-09-10 20:16:28 +0800

    跟新了C++的STL板块目录string

---
+++ 06.C++面向对象编程/01.STL常见应用/02.string/01.string_hz1092.cpp
@@ -0,0 +1,17 @@
+/*************************************************************************
+	> File Name: 01.string_hz1092.cpp
+	> Author: 
+	> Mail: 
+	> Created Time: Sun 10 Sep 2023 08:11:59 PM CST
+ ************************************************************************/
+
+#include<iostream>
+#include<string>
+using namespace std;
+int main() {
+    string s;
+    cin >> s;
+    for (int i = 0; i < s.length(); i++) cout << s[i];
+    cout << endl;
+    return 0;
+}
