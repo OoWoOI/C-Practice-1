26dee3a94b78bde91b2582fdf791812e58bcd5a1
wdd <2437767452@qq.com>
2023-09-10 20:23:01 +0800

    跟新了C++的STL板块目录string1093

---
+++ 06.C++面向对象编程/01.STL常见应用/02.string/02.string_hz1093.cpp
@@ -0,0 +1,16 @@
+/*************************************************************************
+	> File Name: 02.string_hz1093.cpp
+	> Author: 
+	> Mail: 
+	> Created Time: Sun 10 Sep 2023 08:21:14 PM CST
+ ************************************************************************/
+
+#include<iostream>
+#include<string>
+using namespace std;
+int main() {
+    string s;
+    getline(cin, s);
+    cout << s << endl;
+    return 0;
+}
