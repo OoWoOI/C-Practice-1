0be0d49819fd46a7dc2ad6dcdf4b6d6260b7e7ff
wdd <2437767452@qq.com>
2023-09-10 20:29:30 +0800

    跟新了C++的STL板块目录string1094

---
+++ 06.C++面向对象编程/01.STL常见应用/02.string/03.string_hz1094.cpp
@@ -0,0 +1,17 @@
+/*************************************************************************
+	> File Name: 03.string_hz1094.cpp
+	> Author: 
+	> Mail: 
+	> Created Time: Sun 10 Sep 2023 08:26:31 PM CST
+ ************************************************************************/
+
+#include<iostream>
+#include<string>
+using namespace std;
+int main() {
+    string s1, s2, s3;
+    cin >> s1 >> s2;
+    s3 = s1 + s2;
+    cout << s3 << endl;
+    return 0;
+}
