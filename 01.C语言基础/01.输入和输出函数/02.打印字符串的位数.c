3147acbfb5ee514b79f52adc9be50663becfaa9c
wdd <2437767452@qq.com>
2023-09-13 21:47:39 +0800

    C语言基础修改了目录

---
+++ 01.C语言基础/01.输入和输出函数/02.打印字符串的位数.c
@@ -0,0 +1,14 @@
+/*************************************************************************
+	> File Name: 02.打印字符串的位数.c
+	> Author: 
+	> Mail: 
+	> Created Time: Wed 13 Sep 2023 09:39:47 PM CST
+ ************************************************************************/
+
+#include<stdio.h>
+int main() {
+    char str[1000];
+    scanf("%[^\n]s", str);
+    printf(" strlen is %d len\n", printf("%s", str));
+    return 0;
+}
