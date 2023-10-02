2bc49919705b6d8e5250801e762bc65127c8325d
wdd <2437767452@qq.com>
2023-10-02 21:57:08 +0800

    欧拉计划-35 37

--- 02.欧拉计划/EP35.c
+++ 02.欧拉计划/EP35.c
@@ -6,13 +6,15 @@
  ************************************************************************/
 
 #include<stdio.h>
+#include<math.h>
 
-#define max_n 200000
+
+#define max_n 1000000
 #define max_m 1000000
 
 
 //线性筛
-int prime[max_n + 5];
+int prime[max_n + 5] = {0};
 
 void init() {
     for (int i = 2; i < max_m; i++) {
@@ -26,19 +28,51 @@ void init() {
         }
 
     }
-    return 0;
+    return ;
+}
+
+//二分查找
+int binary_search(int *prime, int x) {
+    int l = 1, r = prime[0], mid;
+    while (l <= r) {
+        mid = (l + r) >> 1;
+        if (prime[mid] == x) return mid;
+        if (prime[mid] < x) l = mid + 1;
+        else r = mid - 1;
+    }
+    return -1;
+}
+
+int digits(int num) {
+    return floor(log10(num)) + 1;
+}
+
+int getNewNum(int num) {
+    int x = num % 10;
+    int y = num / 10;
+    return y + x * (int)pow(10, digits(num) - 1);
 }
 
 //判断是否为圆周素数
-
 int is_val(int num) {
-    
-
+    if (num < 10) return 1;
+    int n = digits(num) - 1;
+    while (n--) {
+        num = getNewNum(num);
+        int ret = binary_search(prime, num);
+        if (ret == -1) return 0;
+    }
     return 1;
 }
 
 int main() {
-    for (int )
-
+    init();
+    int sum = 0;
+    for (int i = 1; i <= prime[0]; i++) {
+        if (!is_val(prime[i])) continue;
+        printf("prime[%d] = %d\n", i, prime[i]);
+        sum += 1;
+    }
+    printf("%d\n", sum);
     return 0;
 }
