21f97ded5d269dd87eae0f0c1626198dd5a4da63
wdd <2437767452@qq.com>
2023-10-04 17:38:47 +0800

    cp功能（open与fopen实现）

--- 07.系统编程/02.核心研发课/01.命令行分析/02.ls.c
+++ 07.系统编程/02.核心研发课/01.命令行分析/02.ls.c
@@ -5,13 +5,28 @@
 	> Created Time: Mon 02 Oct 2023 04:36:52 PM CST
  ************************************************************************/
 
-#include<head..h>
+#include<head.h>
 
 //实现ls命令的基本功能：
 // -al -l
 int main(int argc, char *argv[]) {
     int opt;
-    while ((opt = getopt(argc, argv, "a::l:")))  
-
+    while ((opt = getopt(argc, argv, "a::l:"))) {
+        switch (opt) {
+            case 'a': {
+                printf("optind : %d\n", optind);
+                printf("%s\n", optarg);
+            }break;
+            case 'l': {
+                printf("optind : %d\n", optind);
+                printf("%s\n", optarg);
+            } break;
+            default: {
+                perror("open fail !");
+                exit(1);
+            }break;
+        }
+    }  
+    
     return 0;
 }
--- 07.系统编程/02.核心研发课/02.文件与目录基本操作/01.fd.c
+++ 07.系统编程/02.核心研发课/02.文件与目录基本操作/01.fd.c
@@ -6,13 +6,37 @@
  ************************************************************************/
 
 #include<head.h>
+
 int main(int argc, char *argv[]) {
-    int fd;
+    int fd, fd2;
+    if (argc != 3) {
+        fprintf(stderr, "Usage : %s file1 file2 \n", argv[0]);
+        exit(1);
+    } 
+
     if ((fd = open(argv[1], O_RDONLY)) < 0) {
-        fprintf(stderr, "Eroor!\n");
+        fprintf(stderr, "Error!\n");
         perror("open");
         exit(1);
     }
-    perror("open2");
+    
+    if ((fd2 = open(argv[2], O_CREAT | O_RDWR, 0666)) < 0) {
+        perror("open");
+        exit(1);
+    } 
+
+    while (1) {
+        char buff[512] = {0};
+        ssize_t size = read(fd, buff, sizeof(buff));
+        printf("%s", buff);
+        write(fd2, buff, size);
+        if (size <= 0) {
+            break;
+        }
+        //bzero是什么？？？
+
+    }
+    close(fd);
+    close(fd2);
     return 0;
 }
