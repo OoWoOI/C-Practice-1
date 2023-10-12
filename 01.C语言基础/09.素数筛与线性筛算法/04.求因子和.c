f2d1cbd35be51dfe05a9b358360ace341405268b
wdd <2437767452@qq.com>
2023-10-12 21:01:51 +0800

    更新了求因子和

--- 01.C语言基础/09.素数筛与线性筛算法/04.求因子和.c
+++ 01.C语言基础/09.素数筛与线性筛算法/04.求因子和.c
@@ -6,4 +6,29 @@
  ************************************************************************/
 
 #include<stdio.h>
+#define MAX_N 100
 
+int prime[MAX_N + 5] = {1, 1};
+
+void init() {
+    //素数筛
+    for (int i = 2; i <= MAX_N; i++) {
+        if (!prime[i]) {
+            prime[i]  = 1;
+            //continue;
+        }
+        for (int j = i; j <= MAX_N; j += i) {
+            if (prime[j] == 0) prime[j] = i + 1;
+            else prime[j] += i;
+        }
+
+    }
+}
+
+int main() {
+    init();
+    for (int i = 1; i <= MAX_N; i++) {
+        printf("%d\n", prime[i]);
+    }
+    return 0;
+}
