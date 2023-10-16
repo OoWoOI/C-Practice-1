861b63446d87bf4fa156a7a932cc842f0ad2a51b
wdd <2437767452@qq.com>
2023-10-16 21:40:50 +0800

    更新基础数据结构

---
+++ 03.基础数据结构/核心研发/01.vector.cpp
@@ -0,0 +1,29 @@
+/*************************************************************************
+	> File Name: 01.vector.cpp
+	> Author: 
+	> Mail: 
+	> Created Time: Mon 16 Oct 2023 07:13:45 PM CST
+ ************************************************************************/
+
+#include<stdio.h>
+#include<stdlib.h>
+
+
+//线性表：初始化，插入、删除、查找、销毁、
+
+typedef struct Vector{
+    int *data;
+    int size;
+    int len;
+}Vector;
+
+Vector *init(int size) {
+    Vector *v =     
+    return ;
+}
+
+int main() {
+
+
+    return 0;
+}
