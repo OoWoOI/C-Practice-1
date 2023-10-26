b9451063a14f7ec5b5f958de13f37da7723c5aaa
wdd <2437767452@qq.com>
2023-10-26 20:34:16 +0800

    基础数据结构-线索二叉树

--- 03.基础数据结构/核心研发/05.tree-thread-last.cpp
+++ 03.基础数据结构/核心研发/05.tree-thread-last.cpp
@@ -1,10 +1,131 @@
 /*************************************************************************
-	> File Name: 05.tree-thread-last.cpp
+	> File Name: 06.binary_theard_last.c
 	> Author: 
 	> Mail: 
-	> Created Time: Sun 22 Oct 2023 10:36:14 PM CST
+	> Created Time: Fri 01 Sep 2023 07:55:05 PM CST
  ************************************************************************/
 
-#include<iostream>
-using namespace std;
+#include<stdio.h>
+#include<time.h>
+#include<stdlib.h>
 
+#define NORMAL 0
+#define THREAD 1
+
+//二叉线索树的后序遍历
+typedef struct Node {
+    int key;
+    struct Node *lchild, *rchild, *parent;
+    int rtag, ltag;
+}Node;
+
+//获取一个新节点
+Node *getNewNode(int val, Node *root) {
+    Node *p = (Node *)malloc(sizeof(Node));
+    p->key = val;
+    p->lchild = p->rchild = NULL;
+    p->parent = root;
+    p->rtag = p->ltag = NORMAL;
+    return p;
+}
+
+//插入新节点
+Node* insert(Node *root, int val, Node* parent) {
+    if (root == NULL) return getNewNode(val, parent);
+    if (root->key == val) return root;
+    if (root->key < val) root->rchild = insert(root->rchild, val, root);
+    else root->lchild = insert(root->lchild, val, root);
+    return root;
+} 
+
+//线索化
+void bulidThread(Node *root) {
+    if (root == NULL) return ;
+    static Node *pre = NULL;
+    bulidThread(root->lchild);
+    bulidThread(root->rchild);
+    if (root->lchild == NULL) {
+        root->lchild = pre;
+        root->ltag = THREAD;
+    }
+    if (pre && pre->rchild == NULL) {
+        pre->rchild = root;
+        pre->rtag = THREAD;
+    }
+    pre = root;
+    return ;
+}
+
+//销毁
+void clear(Node *root) {
+    if (root == NULL) return ;
+    if (root->ltag == NORMAL) clear(root->lchild);
+    if (root->rtag == NORMAL) clear(root->rchild);
+    free(root);
+    return;
+}
+
+//后序遍历
+void lastOrderByDef(Node *root) {
+    if (root == NULL) return ;
+    if (root->ltag == NORMAL) lastOrderByDef(root->lchild);
+    if (root->rtag == NORMAL) lastOrderByDef(root->rchild);
+    printf("%d ", root->key);
+    return ;
+}
+
+Node* most_left(Node *root) {
+    Node *p = root;
+    while (p && p->ltag == NORMAL && p->lchild) p = p->lchild;
+    return p;
+}
+
+void lastOrderByThread(Node *root) {
+    if (root == NULL) return ;
+     Node *p = root;
+     Node* pre = NULL;
+    while (p) {
+        while (p->lchild != pre) p = p->lchild;
+        while (p && p->rtag == THREAD) {
+            printf("%d ", p->key);
+            pre = p;
+            p = p->rchild;
+        }
+
+        if (p == root && p->rchild == pre) {
+            printf("%d\n", p->key);
+            return ;
+        } 
+
+        while (p && p->rchild == pre) {
+            printf("%d ", p->key);
+            pre = p;
+            p = p->parent;
+        }
+        
+        if (p && p->rtag == NORMAL && p->rchild != pre) {
+            p = p->rchild;
+        }
+    }
+
+    return ;
+}
+
+
+int main() {
+    srand(time(0));
+    #define max_op 20
+    Node *root = NULL;
+    //插入数据
+  //  int ans[10] = {10, 8, 12, 7, 9, 5, 11, 13};
+    for (int i = 0; i < max_op; i++) root = insert(root, rand() % 100, root);
+    //递归实现后序边
+    lastOrderByDef(root), printf("\n");
+    //线索化建立
+    bulidThread(root);
+    lastOrderByThread(root), printf("\n");
+    //销毁树
+    clear(root);
+    #undef max_op
+    return 0;
+}
--- 03.基础数据结构/核心研发/05.tree-thread-pre.cpp
+++ 03.基础数据结构/核心研发/05.tree-thread-pre.cpp
@@ -2,16 +2,131 @@
 	> File Name: 05.tree-thread-pre.cpp
 	> Author: 
 	> Mail: 
-	> Created Time: Sun 22 Oct 2023 10:34:50 PM CST
+	> Created Time: Sun 22 Oct 2023 10:36:01 PM CST
  ************************************************************************/
 
 #include<stdio.h>
 #include<stdlib.h>
+#include<time.h>
 
-//二叉线索树-前序遍历
+#define THREAD 1
+#define NOMORE 0
+
+typedef struct Node {
+    int val;
+    struct Node *left, *right;
+    int rtag, ltag;
+}Node;
+
+//获取新结点
+Node *getNewNode(int val) {
+    Node *node = (Node *)malloc(sizeof(Node));
+    node->val = val;
+    node->left = node->right = NULL;
+    node->rtag = node->ltag = NOMORE;
+    return node;
+}
+
+//插入结点
+Node *insert(Node *root, int val) {
+    if (root == NULL) return getNewNode(val);
+//    if (root->val == val) return root;
+    if (root->val < val) 
+        root->right = insert(root->right, val);
+    else 
+        root->left = insert(root->left, val);
+    return root;
+}
+
+//先序遍历
+void in_order(Node *root) {
+    if (root == NULL) return ;
+    printf("%d ", root->val);
+    in_order(root->left);
+    in_order(root->right);
+    return ;
+}
+
+
+//销毁一棵树
+void clearTree(Node *root) {
+    if (root == NULL) return ;
+    if (root->ltag == NOMORE) clearTree(root->left);
+    if (root->rtag == NOMORE) clearTree(root->right);
+    free(root);
+    return ;
+}
+
+//建立先序遍历线索化
+
+Node *pre = NULL;
+
+void build(Node *root) {
+    if (!root) 
+        return ;
+    
+    //找到最左边的结点
+    if (!root->left) {
+        root->left = pre;
+        root->ltag = THREAD;
+    }
+    if (pre && !pre->right) {
+        pre->right = root;
+        pre->rtag = THREAD;
+    }
+    pre = root;
+    //找到右子树
+   if (root->ltag == NOMORE) build(root->left);
+   if (root->rtag == NOMORE) build(root->right);
+    return ;
+}
+
+//获取当前子树最左边的结点
+Node *getMSTLEFT(Node *p) {
+    while (p && p->ltag == NOMORE && p->left) 
+        p = p->left;
+    return p;
+}
+
+
+//线索化输出
+void output(Node *root) {
+    if (!root) return ;
+    Node *p = root;
+    
+    while (p) {
+        printf("%d ", p->val);
+        if (p->ltag == NOMORE) {
+            p = p->left;
+        } else {
+            p = p->right;
+        }
+    }
+
+    return ;
+}
+
 
 int main() {
-
-
+    srand(time(0));
+    Node *root = NULL;
+    #define MAX_OP 20
+    printf("数据插入: \n");
+    for (int i = 0; i < MAX_OP; i++) {
+        int val = rand() % 100;    
+        root = insert(root, val);
+        i && printf(" ");
+        printf("%d", val);
+    }
+    printf("\n");
+    printf("先序遍历递归: \n");
+    in_order(root), printf("\n"); 
+    
+    //线索化建立
+    build(root);
+    printf("先序遍历线索化: \n");
+    output(root), printf("\n");
+    clearTree(root);
+    #undef MAX_OP
     return 0;
 }
